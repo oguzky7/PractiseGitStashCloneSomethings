cad9cce44d739e61461840f7e8b7d3b9476ef56b
oguzky7 <oguzky7@gmail.com>
2022-12-24 14:19:04 -0500

    2nd commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is my main branch");
+        System.out.println("I'm adding a new feature");
+        System.out.println("Please add new features in new branches");
+        System.out.println("Don't mess with the main branch");
+    }
+}
