efc98836566379aecd9a50332dc9a225e0ba7034
oguzky7 <oguzky7@gmail.com>
2022-12-24 14:44:19 -0500

    push denemesi

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,13 @@ public class Main {
         System.out.println("I'm adding a new feature");
         System.out.println("Please add new features in new branches");
         System.out.println("Don't mess with the main branch");
+
+        System.out.println("This is my code for my new branch that I created");
+        System.out.println("but after I created I forgot to switch to it");
+        /*So, what I did is:
+         git stash
+         git checkout newBranch
+         git stash apply 0
+         */
     }
 }
